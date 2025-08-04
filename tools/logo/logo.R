9c8ce3e30083081f202440b18b44dd19bbfe2e3d
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-08-03 22:57:59 -0600

    Stopped PNG creation

--- tools/logo/logo.R
+++ tools/logo/logo.R
@@ -5,23 +5,23 @@ library(showtext)
 
 img <- "tools/logo/pigeon_sign.png"
 
-sticker(
-  subplot = img,
-  package = "SiGN",
-  s_x = 0.85, s_y = .9, s_width = .9,
-  p_size = 25,
-  p_x = 1.35, p_y = 1.25,
-  p_color = "#2E3A59",
-  h_fill = "#e0e6ed",
-  h_color = "#2E3A59",
-  url = "https://sign-r.github.io/SiGN/",
-  u_x = .225,
-  u_y = .55,
-  u_angle = -30,
-  u_size = 7,
-  dpi = 400,
-  filename = "tools/logo/hex_sticker.png"
-)
+# sticker(
+#   subplot = img,
+#   package = "SiGN",
+#   s_x = 0.85, s_y = .9, s_width = .9,
+#   p_size = 25,
+#   p_x = 1.35, p_y = 1.25,
+#   p_color = "#2E3A59",
+#   h_fill = "#e0e6ed",
+#   h_color = "#2E3A59",
+#   url = "https://sign-r.github.io/SiGN/",
+#   u_x = .225,
+#   u_y = .55,
+#   u_angle = -30,
+#   u_size = 7,
+#   dpi = 400,
+#   filename = "tools/logo/hex_sticker.png"
+# )
 
 sticker(
   subplot = img,
@@ -41,7 +41,7 @@ sticker(
   filename = "tools/logo/hex_sticker.svg"
 )
 
-# use_logo("tools/logo/hex_sticker.svg")
+use_logo("tools/logo/hex_sticker.svg")
 
 
 # GitHub Org Logo
